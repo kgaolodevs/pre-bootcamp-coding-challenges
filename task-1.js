f4c9630e04cb98f9786360b06c9e8911eec92260
kgaolodevs <kgaolodev@gmail.com>
2020-11-10 17:18:39 +0200

    Completed task-1 exercise

---
+++ task-1.js
@@ -0,0 +1,25 @@
+/*
+  Convert pseudocode to actual code
+
+  x = 0
+  y = 1
+  Print the value of x
+  Print the value of y
+  x = x + 3
+  y = y + x
+  Print the value of x
+  Print the value of y
+
+*/
+
+var x = 0;
+var y = 1;
+
+console.log(x);
+console.log(y);
+
+x = x + 3;
+y = y + x;
+
+console.log(x);
+console.log(y);
