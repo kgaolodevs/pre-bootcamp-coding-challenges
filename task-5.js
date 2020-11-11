0f54c0683ae9b0871693abeb7cfb7f3aca3565f9
kgaolodevs <kgaolodev@gmail.com>
2020-11-11 20:37:59 +0200

    Cleaned up task-5 code

--- task-5.js
+++ task-5.js
@@ -4,14 +4,15 @@
   The function should return the area of a triangle.
 */
 
-var calculateArea = function (side1, side2, side3) {
+function calculateArea(side1, side2, side3) {
+  // Formula is calculated first using semi-perimeter and a part of the heron formula
   var semiPerimeter = 0.5 * (side1 + side2 + side3);
   var heronFormula =
     semiPerimeter *
     ((semiPerimeter - side1) *
       (semiPerimeter - side2) *
       (semiPerimeter - side3));
-  var area = Math.sqrt(heronFormula);
-  if (!area) return "Impossible triangle";
+  var area = Math.sqrt(heronFormula); // Result is square rooted
+  if (!area) return "Impossible triangle"; // Will run if numbers cannot produce a realistic triangle
   return area;
-};
+}
