6b0224ac4473cfec60f9506d489690aadd962c7a
kgaolodevs <kgaolodev@gmail.com>
2020-11-11 20:33:49 +0200

    Corrected task-4 solution

--- task-4.js
+++ task-4.js
@@ -1,10 +1,11 @@
 /*
-  Write a function that takes 2 numbers as input. 
-  If either of the numbers is 65, OR if the sum of the numbers is 65 then return true.
-  Otherwise return false.
+  Write a function that takes 2 numbers as input.
+  If either of the numbers is 3 AND the sum of the numbers contains a 3 then return true.
+  Otherwise return false
 */
 
-var myFunction = function (a, b) {
-  if (a === 65 || b === 65 || a + b === 65) return true;
+function myFunction(num1, num2) {
+  var sum = num1 + num2;
+  if (num1 === 3 || num2 === 3 || sum.toString().indexOf("3") > -1) return true;
   return false;
-};
+}
